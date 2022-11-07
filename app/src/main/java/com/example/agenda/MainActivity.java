eec7a7e60a213a7721f4b3fe2ca6d6d1d1019190
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-07 19:28:42 +0100

    Se crean los archivos para la aplicación de la agenda

---
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView lista;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lista=findViewById(R.id.lista);
+        b1=findViewById(R.id.b1);
+    }
+}

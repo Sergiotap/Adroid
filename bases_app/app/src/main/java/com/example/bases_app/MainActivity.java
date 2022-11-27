917d29d38e8a9fdb782dea85b88e45fee3ac69e1
Sergio <Sergiotapiarodr@gmail.com>
2022-11-28 00:01:39 +0100

    Javi cállate

--- bases_app/app/src/main/java/com/example/bases_app/MainActivity.java
+++ bases_app/app/src/main/java/com/example/bases_app/MainActivity.java
@@ -5,23 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-Button add;
-Button reload;
-Button delete;
-Button edit;
-EditText nombre;
-EditText phone;
-EditText nuevoNombre;
-
+Button modificar;
+Button visualizar;
 appDataBase db;
 userDAO usuarioDao;
 private ArrayList<users>userlist;
@@ -30,20 +25,16 @@ userAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intento2 = getIntent();
         setContentView(R.layout.activity_main);
         userlist=new ArrayList<>();
         getSupportActionBar().hide();
-        add=findViewById(R.id.bAdd);
-        reload=findViewById(R.id.bReload);
-        delete=findViewById(R.id.bEliminar);
-        edit=findViewById(R.id.bEditar);
-        nombre=findViewById(R.id.tNombre);
-        phone=findViewById(R.id.tPhone);
-        nuevoNombre=findViewById(R.id.tNuevoNombre);
+        visualizar=findViewById(R.id.bMostrar);
+        modificar=findViewById(R.id.bModificaciones);
         db = Room.databaseBuilder(getApplicationContext(),
                 appDataBase.class, "usuarios").allowMainThreadQueries().build();
         usuarioDao = db.usuarioDao();
-        reload.setOnClickListener(new View.OnClickListener() {
+        visualizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 List<user> usuarios = usuarioDao.getAll();
@@ -55,47 +46,14 @@ userAdapter adapter;
                 createRecycler();
             }
         });
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                userlist.clear();
-                user a = new user();
-                a.nombre = nombre.getText().toString();
-                a.telefono = phone.getText().toString();
-                usuarioDao.insertAll(a);
-            }
-        });
-        //Estos 2 métodos no funcionan correctamente por alguna razón.
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View V) {
-                userlist.clear();
-                List<user> usuarios = usuarioDao.getAll();
-                for(user a : usuarios ){
-                    if(nombre.getText().toString().equals(a.nombre)){
-                        usuarioDao.delete(a);
-                    }
-                }
-            }
-        });
-
-        edit.setOnClickListener(new View.OnClickListener() {
+        modificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userlist.clear();
-                List<user> usuarios = usuarioDao.getAll();
-                for (user a : usuarios){
-                    if(nombre.getText().toString().equals(a.nombre)){
-                        usuarioDao.delete(a);
-                        user aNuevo = new user();
-                        aNuevo.nombre = nuevoNombre.getText().toString();
-                        aNuevo.telefono = phone.getText().toString();
-                        usuarioDao.insertAll(aNuevo);
-                    }
-                }
+                Toast.makeText(getApplicationContext(), "funciono",Toast.LENGTH_SHORT).show();
+                //lanzar();
             }
         });
+
     }
     public void createRecycler(){
         //Crea el ArrayList, obtiene el id del Recycler, obtiene el layout del dispositivo
@@ -106,5 +64,9 @@ userAdapter adapter;
         adapter = new userAdapter(userlist);
         //Anades el adaptador al dispositivo
         recyclerUsuarios.setAdapter(adapter);
+    }
+    public void lanzar(){
+        Intent intento1 = new Intent(this,modUsuarios.class);
+        startActivity(intento1);
     }
 }
---
+++ bases_app/app/src/main/java/com/example/bases_app/modUsuarios.java
@@ -0,0 +1,86 @@
+package com.example.bases_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import java.util.List;
+
+public class modUsuarios extends AppCompatActivity {
+    Button add;
+    Button editar;
+    Button delete;
+    Button ver;
+    EditText nombre;
+    EditText phone;
+    EditText nuevoNombre;
+    appDataBase db;
+    userDAO usuarioDao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intento1 = getIntent();
+        setContentView(R.layout.mod_usuarios);
+        add=findViewById(R.id.bAdd);
+        editar=findViewById(R.id.bEditar);
+        delete=findViewById(R.id.bEliminar);
+        ver=findViewById(R.id.bVisualizar);
+        nombre=findViewById(R.id.tNombre);
+        phone=findViewById(R.id.tPhone);
+        nuevoNombre=findViewById(R.id.tNuevoNombre);
+        db = Room.databaseBuilder(getApplicationContext(),
+                appDataBase.class, "usuarios").allowMainThreadQueries().build();
+        usuarioDao = db.usuarioDao();
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user a = new user();
+                a.nombre = nombre.getText().toString();
+                a.telefono = phone.getText().toString();
+                usuarioDao.insertAll(a);
+            }
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                List<user> usuarios = usuarioDao.getAll();
+                for(user a : usuarios ){
+                    if(nombre.getText().toString().equals(a.nombre)){
+                        usuarioDao.delete(a);
+                    }
+                }
+            }
+        });
+
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<user> usuarios = usuarioDao.getAll();
+                for (user a : usuarios){
+                    if(nombre.getText().toString().equals(a.nombre)){
+                        usuarioDao.delete(a);
+                        user aNuevo = new user();
+                        aNuevo.nombre = nuevoNombre.getText().toString();
+                        aNuevo.telefono = phone.getText().toString();
+                        usuarioDao.insertAll(aNuevo);
+                    }
+                }
+            }
+        });
+        ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzar();
+            }
+        });
+    }
+    public void lanzar(){
+        Intent intento2 = new Intent(this,MainActivity.class);
+        startActivity(intento2);
+    }
+}
